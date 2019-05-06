347372ec1bec37ecd189dc291d32a1c5835e58e3
ttsia <ttsia@softserveinc.com>
2019-05-06 14:19:40 +0300

    add basic config structure

---
+++ settings/base.py
@@ -0,0 +1,19 @@
+import os
+
+
+class Config(object):
+    DEBUG = False
+
+    MONGO_DATABASES = {
+        "app": {
+            "username": os.environ.get('MONGO_USER'),
+            "password": os.environ.get('MONGO_PASSWORD'),
+            "port": 27017,
+            "host": 'mongodb+srv://{}:{}@{}/{}'.format(
+                os.environ.get('MONGO_USER'),
+                os.environ.get('MONGO_PASSWORD'),
+                os.environ.get('MONGO_HOST'),
+                os.environ.get('MONGO_DATABASE')
+            ),
+        }
+    }
