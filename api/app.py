691ed49f86e82a4c5b84eab64b17aa8c972fd9b9
ne-luboff <ne-luboff@yandex.ua>
2019-05-06 14:49:34 +0300

    Add users blueprint

--- api/app.py
+++ api/app.py
@@ -1,3 +1,4 @@
+from api.users.urls import user_blueprint
 from flask import Flask
 from flask_restful import Api
 from api.common import views as common_views
@@ -5,10 +6,10 @@ from api.common import views as common_views
 APP = Flask(__name__)
 API = Api(APP)
 
+APP.register_blueprint(user_blueprint)
+
 # common app views
 API.add_resource(
     common_views.Index,
     '/'
 )
-
-# users app views
