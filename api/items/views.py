76a7aad14919ca8520ecd4c5cb2647dda0fec487
ttsia <ttsia@softserveinc.com>
2019-05-07 14:14:34 +0300

    rename interface. show error messages

--- api/items/views.py
+++ api/items/views.py
@@ -1,7 +1,7 @@
 from flask_restful import Resource
-from flask import request, Response
+from flask import request, Response, jsonify
 from bson.json_util import dumps
-from database import interface
+from database import items
 
 # TODO: return error messages
 
@@ -10,17 +10,35 @@ class Item(Resource):
     Shows a single item and lets you delete/update a item.
     """
     def get(self, item_id):
-        response = interface.get_item(item_id)
-        return Response(dumps(response), mimetype='application/json')
+        response = items.get_item(item_id)
+        try:
+            return Response(dumps(response), mimetype='application/json')
+        except Exception as e:
+            response = {
+                "error": str(e)
+            }
+            return Response(dumps(response), status=400, mimetype='application/json')
 
     def delete(self, item_id):
-        response = interface.delete_item(item_id)
-        return Response(response, status=204, mimetype='application/json')
+        items.delete_item(item_id)
+        try:
+            return Response(status=204)
+        except Exception as e:
+            response = {
+                "error": str(e)
+            }
+            return Response(dumps(response), status=400, mimetype='application/json')
 
     def put(self, item_id):
         item_data = request.get_json()
-        response = interface.update_item(item_id, item_data)
-        return Response(dumps(response), mimetype='application/json')
+        response = items.update_item(item_id, item_data)
+        try:
+            return Response(dumps(response), mimetype='application/json')
+        except Exception as e:
+            response = {
+                "error": str(e)
+            }
+            return Response(dumps(response), status=400, mimetype='application/json')
 
 
 class ItemList(Resource):
@@ -28,10 +46,10 @@ class ItemList(Resource):
     Shows a list of all items, and lets you POST to add new items.
     """
     def get(self):
-        response = interface.get_all_items()
+        response = items.get_all_items()
         return Response(dumps(response), mimetype='application/json')
     
     def post(self):
         item_data = request.get_json()
-        response = interface.create_item(item_data)
+        response = items.create_item(item_data)
         return Response(dumps(response), status=201, mimetype='application/json')
