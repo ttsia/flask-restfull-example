154b0dba5b56f6ad5ff9da4387ff07d31ccdcd12
rddimon <rddimon@gmail.com>
2019-05-08 17:42:54 +0300

    merge and updates

--- tests/test_api_common.py
+++ tests/test_api_common.py
@@ -1,8 +1,3 @@
-import json
-
-
 def test_hello(client):
     response = client.get('/')
     assert response.status_code == 200
-    data = json.loads(response.data.decode())
-    assert data['hello'] == "world"
