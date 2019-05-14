1763c444b10946701840ae69c60f2babed4a1833
rddimon <rddimon@gmail.com>
2019-05-14 15:14:49 +0300

    improvement web app and tests

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from settings import current_config
 from settings.basic_auth import BASIC_AUTH
 from settings.jwt_auth import JWT
 from api.urls import API_BLUEPRINT
+from web.urls import register_web_blueprints
 
 
 def create_app(config=None):
@@ -22,6 +23,9 @@ def create_app(config=None):
     # blueprints registration
     app.register_blueprint(API_BLUEPRINT)
 
+    # web app
+    register_web_blueprints(app)
+
     # jwt initialization
     JWT.init_app(app)
 
@@ -29,5 +33,6 @@ def create_app(config=None):
     BASIC_AUTH.init_app(app)
 
     return app
+
 
 APP = create_app(current_config)
